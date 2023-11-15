555584fd1bb358bce37655a54f6a058835ff3367
Nahom kiflu <kiflunahom1994@gmail.com>
2023-11-16 01:06:07 +0300

    buble sorting algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+/**
+ * bubble_sort - sorts by repeatedly swapping the adjacent
+ * elements if they are in the wrong order
+ *
+ * @array: a pointer to an array to be sorted
+ * @size: size pf the array
+ *
+ * Returns: nothing(void)
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	bool swaped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swaped = false;
+
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				swaped = true;
+			}
+		}
+		if (swaped == false)
+			break;
+
+	}
+}
+/**
+ * swap - swaps two pointers
+ *
+ * @x: pointer to be swaped
+ * @y: pointer to be swaped
+ *
+ * Return: noting (void)
+ */
+void swap(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
