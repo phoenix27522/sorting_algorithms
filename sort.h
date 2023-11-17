b530d03f13d37233ee000585e8ecd5db463f8f48
Nahom kiflu <kiflunahom1994@gmail.com>
2023-11-17 22:52:37 +0300

    quick sort

--- sort.h
+++ sort.h
@@ -7,6 +7,10 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
 
 /**
  * struct listint_s - Doubly linked list node
