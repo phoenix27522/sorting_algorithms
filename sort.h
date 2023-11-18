2c83dc0f37a33664cca378b88556d440b385e7e7
Nahom kiflu <kiflunahom1994@gmail.com>
2023-11-18 23:02:21 +0300

    merge sort

--- sort.h
+++ sort.h
@@ -33,6 +33,7 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 /* ----------------------- lomuto algorithm ------------------*/
 int lomuto_partition(int *array, size_t size, int left, int right);
