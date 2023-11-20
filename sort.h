217e492e74ae67bea5953529c6c7a4af46a2cd35
Nahom kiflu <kiflunahom1994@gmail.com>
2023-11-21 00:04:55 +0300

    quick_sort

--- sort.h
+++ sort.h
@@ -42,6 +42,11 @@ void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+/* ----------------------------- quick_sort ------------------------------*/
+int hoare_partition(int *array, size_t size, int left, int right);
+void hoare_sort(int *array, size_t size, int left, int right);
 
 /* ------------------------------ batonic --------------------------------*/
 void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
